e83c2039b16be23189c91cd84562e68d96e5ba59
ErikaFlores <foglzerika@gmail.com>
2023-10-30 09:11:58 -0600

    feat | init project

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Invoice Page</h1>
+    </div>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+//This file be used to keep the fonts that'll be used throughout 
+//our app
+import {Inter, Lusitana} from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ 
+    weight:'400',
+    subsets: ['latin']
+});
